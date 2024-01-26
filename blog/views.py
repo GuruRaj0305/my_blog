eae3fe31b6bc538488ed28b03142d39a63c45ddd
GuruRaj0305 <gururajhr0305l@gmail.com>
2024-01-26 21:09:25 +0530

    structure of urls and views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import Http404
 
 # Create your views here.
+
+def index(request):
+    return render(request, "blog/main.html")
+
+def posts(request):
+    return
+
+def post_detailed(request):
+    return
