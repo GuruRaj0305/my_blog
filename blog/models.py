3f06e5b5e21f2c5d1a64cdeab0ebd8ffc744015d
GuruRaj0305 <gururajhr0305l@gmail.com>
2024-02-06 03:20:23 +0530

    complete db add

--- blog/models.py
+++ blog/models.py
@@ -6,10 +6,18 @@ from django.core.validators import MinLengthValidator
 class Tag(models.Model):
     caption = models.CharField(max_length = 20)
 
+    def __str__(self):
+        return self.caption
+    
+
 class Author(models.Model):
     first_name = models.CharField(max_length = 100)
     last_name = models.CharField(max_length = 100)
     email = models.EmailField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    
 
 class Post(models.Model):
     title = models.CharField(max_length = 50)
@@ -18,5 +26,5 @@ class Post(models.Model):
     date = models.DateField(auto_now = True)
     slug = models.SlugField(unique = True, db_index = True)
     content = models.TextField(validators = [MinLengthValidator(10)])
-    author = models.ForeignKey(Author, on_delete = models.SETNULL, related_name = "post")
+    author = models.ForeignKey(Author, on_delete = models.SET_NULL, related_name = "post", null = True)
     tags = models.ManyToManyField(Tag)
